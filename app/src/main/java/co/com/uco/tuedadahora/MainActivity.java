1e47a13a72351943674af1fa6bdbb2a176c0eed8
juanftech <juan.restrepo@techandsolve.com>
2018-08-20 20:37:08 -0500

    Se agrega manejo de excepciones para cuando no se diligencia algun campo

--- app/src/main/java/co/com/uco/tuedadahora/MainActivity.java
+++ app/src/main/java/co/com/uco/tuedadahora/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -69,35 +70,45 @@ public class MainActivity extends AppCompatActivity {
 
     public void calcularEdad(View view) throws TuEdadAhoraExcepcion {
 
-        String nombre = txtNombre.getText().toString();
-        txtNombre.setText("");
-        int edadEnAnios = obtenerEdadEnAnios();
-        Intent intent = new Intent(MainActivity.this, EdadActivity.class);
-        intent.putExtra(Constantes.EDAD, String.valueOf(edadEnAnios));
-        intent.putExtra(Constantes.NOMBRE, nombre);
-        startActivity(intent);
+        try {
+            String nombre = txtNombre.getText().toString();
+            int edadEnAnios = obtenerEdadEnAnios();
+            if (nombre.isEmpty()) throw new TuEdadAhoraExcepcion(getString(R.string.error_campos_vacios));
+            Intent intent = new Intent(MainActivity.this, EdadActivity.class);
+            intent.putExtra(Constantes.EDAD, String.valueOf(edadEnAnios));
+            intent.putExtra(Constantes.NOMBRE, nombre);
+            txtNombre.setText("");
+            dateFeNacimiento.setText("");
+            startActivity(intent);
+
+        } catch (TuEdadAhoraExcepcion excepcion) {
+            Toast.makeText(getApplicationContext(), getString(R.string.error_campos_vacios), Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), getString(R.string.error_campos_vacios), Toast.LENGTH_SHORT).show();
+        }
 
     }
 
     private int obtenerEdadEnAnios() throws TuEdadAhoraExcepcion {
 
         SimpleDateFormat dateFormat = new SimpleDateFormat(Constantes.FORMATO_FECHA);
-        Date fechaDeNacimiento;
+        Date fechaDeNacimiento = null;
+        int anios = 0;
         try {
             fechaDeNacimiento = dateFormat.parse(dateFeNacimiento.getText().toString());
-        } catch (ParseException e) {
-            throw new TuEdadAhoraExcepcion(getString(R.string.error_formato_fecha), e);
+            Calendar feNacimiento = new GregorianCalendar();
+            feNacimiento.setTime(fechaDeNacimiento);
+            Calendar today = Calendar.getInstance();
+            anios = today.get(Calendar.YEAR) - feNacimiento.get(Calendar.YEAR);
+            int meses = today.get(Calendar.MONTH) - feNacimiento.get(Calendar.MONTH);
+            int dias = today.get(Calendar.DAY_OF_MONTH) - feNacimiento.get(Calendar.DAY_OF_MONTH);
+            if ( meses < 0 || (meses == 0 && dias < 0 )) {
+                anios = anios - 1;
+            }
+        } catch (Exception e) {
+            throw new TuEdadAhoraExcepcion(getString(R.string.error_campos_vacios), e);
         }
-        Calendar feNacimiento = new GregorianCalendar();
-        feNacimiento.setTime(fechaDeNacimiento);
-        Calendar today = Calendar.getInstance();
-        int anios = today.get(Calendar.YEAR) - feNacimiento.get(Calendar.YEAR);
-        int meses = today.get(Calendar.MONTH) - feNacimiento.get(Calendar.MONTH);
-        int dias = today.get(Calendar.DAY_OF_MONTH) - feNacimiento.get(Calendar.DAY_OF_MONTH);
-        if ( meses < 0 || (meses == 0 && dias < 0 )) {
-            anios = anios - 1;
-        }
-        dateFeNacimiento.setText("");
+
         return anios;
     }
 }
